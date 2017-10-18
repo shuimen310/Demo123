e6cce0f45a0c87b9061b4d0577d505c03323800c
chenjun <514530114@qq.com>
2017-10-18 10:47:44 +0800

    haha

---
+++ test/src/main/java/demo.java
@@ -0,0 +1,19 @@
+/**
+ * Created by chenjun on 2017/7/8.
+ */
+public class demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("萍萍是猪---"+(i+1));
+
+
+        }
+
+    }
+
+
+
+
+
+
+}
